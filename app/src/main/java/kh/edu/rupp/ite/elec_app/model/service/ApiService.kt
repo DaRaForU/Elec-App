e1e203125b94bd0dc69bd2fdccb3ad28d510218c
DaRaForU <san.sovandara.fe@gmail.com>
2023-12-10 23:00:16 +0700

    Apply suspend function in ApiService

--- app/src/main/java/kh/edu/rupp/ite/elec_app/model/service/ApiService.kt
+++ app/src/main/java/kh/edu/rupp/ite/elec_app/model/service/ApiService.kt
@@ -8,8 +8,8 @@ import retrofit2.http.GET
 interface ApiService {
 
     @GET("/98J3VH/data")
-    fun loadListProducts1(): Call<List<ListProducts1>>;
+    suspend fun loadListProducts1(): List<ListProducts1>;
 
     @GET("/wUJZyi/data")
-    fun loadListProducts2(): Call<List<ListProducts2>>;
+    suspend fun loadListProducts2(): List<ListProducts2>;
 }
--- app/src/main/java/kh/edu/rupp/ite/elec_app/viewmodel/ProductsViewModel.kt
+++ app/src/main/java/kh/edu/rupp/ite/elec_app/viewmodel/ProductsViewModel.kt
@@ -1,15 +1,20 @@
 package kh.edu.rupp.ite.elec_app.viewmodel
 
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kh.edu.rupp.ite.elec_app.model.client.ApiClient
 import kh.edu.rupp.ite.elec_app.model.model.ApiData
 import kh.edu.rupp.ite.elec_app.model.model.ListProducts1
 import kh.edu.rupp.ite.elec_app.model.model.ListProducts2
 import kh.edu.rupp.ite.elec_app.model.model.Status
 import kh.edu.rupp.ite.elec_app.model.service.ApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,15 +31,31 @@ class ProductsViewModel: ViewModel() {
     val apiClient = ApiClient.get();
 
     fun showProducts1(){
-        val apiData = ApiData<List<ListProducts1>>(Status.PROCESSING, null);
+        var apiData = ApiData<List<ListProducts1>>(Status.PROCESSING, null);
         _products1Data.postValue(apiData);
 
 
         //Create api Service
-        val apiService = apiClient.apiServince;
+        val apiService = apiClient.apiService;
 
         //Load ListProduct1 from server
-        val task: Call<List<ListProducts1>> = apiService.loadListProducts1();
+        viewModelScope.launch(Dispatchers.IO) {
+            //Processing in the background
+            apiData = try {
+                val response = apiService.loadListProducts1();
+                ApiData<List<ListProducts1>>(Status.SUCCESS, response);
+            }catch (ex: Exception){
+                Log.e("ited", "Loading Products1 failed!: ${ex.message}");
+                ApiData<List<ListProducts1>>(Status.ERROR, null);
+            }
+
+            //Processing out the background
+            withContext(Dispatchers.Main.immediate){
+                _products1Data.postValue(apiData);
+            }
+        }
+
+        /*val task: Call<List<ListProducts1>> = apiService.loadListProducts1();
         task.enqueue(object : Callback<List<ListProducts1>> {
             override fun onResponse(
                 call: Call<List<ListProducts1>>,
@@ -57,6 +78,7 @@ class ProductsViewModel: ViewModel() {
             }
 
         })
+         */
     }
 
 
@@ -65,30 +87,27 @@ class ProductsViewModel: ViewModel() {
     val products2Data: LiveData<ApiData<List<ListProducts2>>>
         get() = _products2Data
     fun showProducts2(){
+        var apiData = ApiData<List<ListProducts2>>(Status.PROCESSING, null);
+        _products2Data.postValue(apiData);
 
-        val apiService = apiClient.apiServince;
+        //Create api Service
+        val apiService = apiClient.apiService;
 
         //Load ListProduct1 from server
-        val task: Call<List<ListProducts2>> = apiService.loadListProducts2();
-        task.enqueue(object : Callback<List<ListProducts2>>{
-            override fun onResponse(
-                call: Call<List<ListProducts2>>,
-                response: Response<List<ListProducts2>>
-            ) {
-                if(response.isSuccessful){
-                    val apiData = ApiData<List<ListProducts2>>(Status.SUCCESS, response.body());
-                    _products2Data.postValue(apiData);
-                }else{
-                    val apiData = ApiData<List<ListProducts2>>(Status.ERROR, null);
-                    _products2Data.postValue(apiData);
-                }
+        viewModelScope.launch(Dispatchers.IO) {
+            //Processing in the background
+            apiData = try {
+                val response = apiService.loadListProducts2();
+                ApiData<List<ListProducts2>>(Status.SUCCESS, response);
+            }catch (ex: Exception){
+                Log.e("ited", "Loading Products2 failed!: ${ex.message}");
+                ApiData<List<ListProducts2>>(Status.ERROR, null);
             }
 
-            override fun onFailure(call: Call<List<ListProducts2>>, t: Throwable) {
-                val apiData = ApiData<List<ListProducts2>>(Status.ERROR, null);
+            //Processing out the background
+            withContext(Dispatchers.Main.immediate){
                 _products2Data.postValue(apiData);
             }
-
-        })
+        }
     }
 }
